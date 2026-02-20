120950c1856289ae8c0b044bfe6b9a86a831c386
simonrulof <simonber2001@gmail.com>
2026-02-20 14:28:27 +0100

    feat(header): add header component with language toggle and styling refactor(game): remove header from game component and adjust styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { GameComponent } from './components/game/game.component';
 import { CookieConsentComponent } from './components/cookie-consent/cookie-consent.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [GameComponent, CookieConsentComponent],
+  imports: [GameComponent, CookieConsentComponent, HeaderComponent],
   template: `
     <app-cookie-consent></app-cookie-consent>
+    <app-header></app-header>
     <app-game></app-game>
   `,
   styleUrl: './app.scss'
